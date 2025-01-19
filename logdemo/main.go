4c636ddd7ea3f57fb3b4f1893f700ff353b75a84
noone <noreplay@gmail.com>
2025-01-19 23:23:14 +0800

    logdemo

--- logdemo/main.go
+++ logdemo/main.go
@@ -36,12 +36,12 @@ func main() {
 
 	for i := 0; i < 10; i++ {
 		wg.Add(1)
-		go func() {
-			defer wg.Done()
+		go func(w *sync.WaitGroup) {
+			defer w.Done()
 			for j := 0; j < 100; j++ {
 				logger.Info("gorutine", "id", i, "value", j)
 			}
-		}()
+		}(&wg)
 	}
 	wg.Wait()
 
