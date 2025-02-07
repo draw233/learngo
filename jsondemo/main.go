5a09bf31ef29ac027c14726a0f3fb7b669bf402d
noone <noreplay@gmail.com>
2025-02-07 22:27:42 +0800

    http demo

--- jsondemo/main.go
+++ jsondemo/main.go
@@ -1,30 +1,64 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"jsondemo/model"
 	"log"
 	"net/http"
+	"net/url"
 	"strconv"
+	"strings"
 	"time"
 )
 
 var (
-	url = "https://api.juejin.cn/interact_api/v1/pin_tab_lead?aid=2608&uuid=7347226625274201636&spider=0"
+	urlx = "https://api.juejin.cn/interact_api/v1/pin_tab_lead?aid=2608&uuid=7347226625274201636&spider=0"
 )
 
 func main() {
-	timeClickDemo()
+	fmt.Println("1")
+	HttpPostFormDemo()
+}
+
+func HttpPostFormDemo() {
+	var param = url.Values{}
+	param.Set("name", "你")
+	param.Set("password", "pass")
+	xxx := param.Encode()
+	fmt.Println(xxx)
+	resp, err := http.Post("http://127.0.0.1:4523/m1/5825518-5510942-default/demo/login?apifoxApiId=258519976", "application/x-www-form-urlencoded", strings.NewReader(xxx))
+	if err != nil {
+		log.Fatal("xx")
+	}
+	defer resp.Body.Close()
+
+	xx, _ := io.ReadAll(resp.Body)
+	fmt.Println(string(xx))
+}
+
+func HttpPostJsonDemo() {
+
+	resp, err := http.Post("http://127.0.0.1:4523/m1/5825518-5510942-default/demo/login?apifoxApiId=258520620", "application/json", bytes.NewReader([]byte(`{"name":"a","passwor":"xx"}`)))
+	if err != nil {
+		log.Fatal("xx")
+	}
+	defer resp.Body.Close()
+
+	xx, _ := io.ReadAll(resp.Body)
+	fmt.Println(string(xx))
 }
 
 func HttpGetDemo() {
-	req, err := http.NewRequest(http.MethodGet, url, nil)
+	req, err := http.NewRequest(http.MethodGet, urlx, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	req.Header.Set("h1", "v1")
 	var data model.Resp
 
 	resp, err := http.DefaultClient.Do(req)
@@ -84,7 +118,7 @@ func newFunction(context context.Context) {
 	for {
 		select {
 		case <-ticker.C:
-			fmt.Println("do work ",time.Since(now))
+			fmt.Println("do work ", time.Since(now))
 		case <-context.Done():
 			fmt.Println("done")
 			return
